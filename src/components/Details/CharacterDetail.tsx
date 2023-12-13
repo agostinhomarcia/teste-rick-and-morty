dd235917200a2ac080d7780506b68db47f278e81
Márcia Agostinho <106019020+agostinhomarcia@users.noreply.github.com>
2023-12-13 19:18:52 -0300

    feat(personagem): adiciona características específicas do personagem

--- src/components/Details/CharacterDetail.tsx
+++ src/components/Details/CharacterDetail.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { View, Text, Image } from "react-native";
 import { CharacterDetails } from "../../types/types";
 import { StyledImage, StyledText, StyledView } from "./styles";
+import { useQuery } from "@apollo/client";
+import { GET_CHARACTER_DETAILS } from "../../graphql/graphql";
 
 interface CharacterDetailProps {
   character: CharacterDetails;
@@ -9,33 +11,45 @@ interface CharacterDetailProps {
 
 const CharacterDetail: React.FC<CharacterDetailProps> = (props) => {
   const { character } = props;
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(GET_CHARACTER_DETAILS, {
+    variables: { id: id },
+  });
 
-  if (
-    !character.status ||
-    !character.species ||
-    !character.episode ||
-    !character.location
-  ) {
+  if (loading) {
     return (
       <View>
-        <Text>Detalhes do personagem não disponíveis.</Text>
+        <Text>Carregando...</Text>
       </View>
     );
   }
 
+  if (error || !data || !data.character) {
+    return (
+      <View>
+        <Text>Erro ao carregar detalhes do personagem.</Text>
+      </View>
+    );
+  }
+
+  const characterData = data.character;
+
   return (
     <StyledView>
       <StyledImage
-        source={{ uri: character.image }}
+        source={{ uri: characterData.image }}
         style={{ width: 200, height: 200 }}
       />
-      <StyledText>Name: {character.name}</StyledText>
-      <StyledText>Status: {character.status}</StyledText>
-      <StyledText>Species: {character.species}</StyledText>
-      <StyledText>Episode: {character.episode}</StyledText>
-      <StyledText>Location: {character.location}</StyledText>
+      <StyledText>Name: {characterData.name}</StyledText>
+      <StyledText>Status: {characterData.status}</StyledText>
+      <StyledText>Species: {characterData.species}</StyledText>
+      <StyledText>Episode: {characterData.episode}</StyledText>
+      <StyledText>Location: {characterData.location}</StyledText>
     </StyledView>
   );
 };
 
 export default CharacterDetail;
+function useParams(): { id: any } {
+  throw new Error("Function not implemented.");
+}
